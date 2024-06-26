3c9943c409136b1eb8ddc0d4421a8dffa59106e9
Nahima-ayni <nahimamadey1@gmail.com>
2024-06-26 19:55:55 +0300

    ES6 creating a promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,6 @@
 export default function getResponseFromAPI() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            console.log('So, what brings you here?');
 	    resolve('success');
         }, 3000);
     })
