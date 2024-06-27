cb1397a330f8819a62ef456f2fc1843586d09ed2
Nahima-ayni <nahimamadey1@gmail.com>
2024-06-27 21:37:19 +0300

    classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,8 +3,24 @@ export default class Airport {
     this._name = name;
     this._code = code;
   }
-	
-  toString() {
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
     return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
   }
 }
