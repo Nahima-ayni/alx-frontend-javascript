a43efb5e529276bd5f954698a1e096b129fb2ed2
Nahima-ayni <nahimamadey1@gmail.com>
2024-06-27 14:36:15 +0300

    ES6 creating a promise

--- 0x02-ES6_classes/0x01-ES6_promise/0-promise.js
+++
@@ -1,7 +0,0 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-	    resolve('success');
-        }, 3000);
-    })
-}
--- 0x02-ES6_classes/0x01-ES6_promise/0x00-ES6_basic/100-createIteratorObject.js
+++
@@ -1,8 +0,0 @@
-export default function createIteratorObject(report) {
-    let employees = [];
-    for (let department in report.allEmployees) {
-        employees = employees.concat(report.allEmployees[department]);
-    }
-
-    return employees[Symbol.iterator]();
-}
