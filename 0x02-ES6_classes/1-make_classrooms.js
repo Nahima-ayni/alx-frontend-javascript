3235e834a35e8186cabf709c74ab952d5bed0561
Nahima-ayni <nahimamadey1@gmail.com>
2024-06-27 15:28:15 +0300

    ES6 creating a promise

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,6 +4,7 @@ export default function initializeRooms() {
   const size1 = new ClassRoom(19);
   const size2 = new ClassRoom(20);
   const size3 = new ClassRoom(34);
+  const size_all = [size1, size2, size3];
 
-  return [size1, size2, size3];
+  return (size_all);
 }
