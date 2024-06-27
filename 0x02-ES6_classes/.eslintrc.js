cf172c96394aac6cd4731a53aab2f45ec4ec3dc1
Nahima-ayni <nahimamadey1@gmail.com>
2024-06-27 15:31:17 +0300

    classes

--- 0x02-ES6_classes/.eslintrc.js
+++ 0x02-ES6_classes/.eslintrc.js
@@ -1,33 +1,37 @@
 module.exports = {
-  env: {
-    browser: false,
-    es2021: true,
-    jest: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint', 'jest'],
-  rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-  },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
-}
+	env: {
+	  browser: false,
+	  es6: true,
+	  jest: true,
+	},
+	extends: [
+	  'airbnb-base',
+	  'plugin:jest/all',
+	],
+	globals: {
+	  Atomics: 'readonly',
+	  SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+	  ecmaVersion: 2018,
+	  sourceType: 'module',
+	},
+	plugins: ['jest'],
+	rules: {
+	  'max-classes-per-file': 'off',
+	  'no-underscore-dangle': 'off',
+	  'no-console': 'off',
+	  'no-shadow': 'off',
+	  'no-restricted-syntax': [
+		'error',
+		'LabeledStatement',
+		'WithStatement',
+	  ],
+	},
+	overrides:[
+	  {
+		files: ['*.js'],
+		excludedFiles: 'babel.config.js',
+	  }
+	]
+  };
