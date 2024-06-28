273da23d8926d43038c38a994fe849bf4d348a2a
Nahima-ayni <nahimamadey1@gmail.com>
2024-06-28 20:53:29 +0300

    ES6 promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  .then(() => return ({ status: 200, body: 'success' }))
-  .catch(() => throw Error())
-  .finally(() => {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => throw Error())
+    .finally(() => {
     console.log('Got a response from the API')
   });
 }
